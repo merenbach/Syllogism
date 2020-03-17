0b4f6140f470ef5a080b46eeda41ec2b3e8ec0ce
Andrew Merenbach <andrew@merenbach.com>
2020-03-17 00:55:49 -0700

    Invert boolean for article type none

--- syllogism.go
+++ syllogism.go
@@ -478,10 +478,10 @@ func basicGosub6200() {
 					return fmt.Sprintf("All %s is %s", symbolTable.Symbols[localint_c2].Term, symbolTable.Symbols[localint_c1].Term)
 				}
 			} else {
-				if symbolTable.Symbols[localint_c1].ArticleType != article.TypeNone || symbolTable.Symbols[localint_c2].ArticleType == article.TypeNone {
-					return fmt.Sprintf("Some %s is %s%s", symbolTable.Symbols[localint_c1].Term, symbolTable.Symbols[localint_c2].ArticleType, symbolTable.Symbols[localint_c2].Term)
-				} else {
+				if symbolTable.Symbols[localint_c1].ArticleType == article.TypeNone && symbolTable.Symbols[localint_c2].ArticleType != article.TypeNone {
 					return fmt.Sprintf("Some %s is %s%s", symbolTable.Symbols[localint_c2].Term, symbolTable.Symbols[localint_c1].ArticleType, symbolTable.Symbols[localint_c1].Term)
+				} else {
+					return fmt.Sprintf("Some %s is %s%s", symbolTable.Symbols[localint_c1].Term, symbolTable.Symbols[localint_c2].ArticleType, symbolTable.Symbols[localint_c2].Term)
 				}
 			}
 
