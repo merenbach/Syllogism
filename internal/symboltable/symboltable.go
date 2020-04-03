afa55a60e9f8538b21616b6429af8cc223ae7a1e
Andrew Merenbach <andrew@merenbach.com>
2020-04-02 22:39:12 -0700

    Add some commented methods to symboltable

--- internal/symboltable/symboltable.go
+++ internal/symboltable/symboltable.go
@@ -45,6 +45,35 @@ func (st *SymbolTable) Iterate(start int, f func(int, *symbol.Symbol) bool) {
 	}
 }
 
+// // Prune orphaned terms with no occurrences.
+// func (st *SymbolTable) Prune() {
+// 	var ss []*symbol.Symbol
+// 	for i := 1; i <= st.HighestLocationUsed(); i++ {
+// 		s := st.Symbols[i]
+// 		if s.Occurrences > 0 {
+// 			ss = append(ss, s)
+// 		}
+// 	}
+
+// 	st.Symbols = ss
+// }
+
+// // Delete a term from the table.
+// func (st *SymbolTable) Delete(sym *symbol.Symbol) {
+// 	for i, s := range st.Symbols {
+// 		if s.Term == sym.Term {
+// 			// Delete without leaving uncollected pointers
+// 			// https://github.com/golang/go/wiki/SliceTricks
+// 			if i < len(st.Symbols)-1 {
+// 				copy(st.Symbols[i:], st.Symbols[i+1:])
+// 			}
+// 			st.Symbols[len(st.Symbols)-1] = nil
+// 			st.Symbols = st.Symbols[:len(st.Symbols)-1]
+// 			break
+// 		}
+// 	}
+// }
+
 // Search a symbol table for a term matching a given string.
 // Porting notes: All variable use is encapsulated, so if porting needs to be re-done in future, re-porting this function can be avoided by invoking the equivalent of `i1, b1 = search(start, w$)`.
 func (st *SymbolTable) Search(start int, w string) (int, int) {
