1c81038494a4935a3cbf1ccfbc7385a47794f494
Andrew Merenbach <andrew@merenbach.com>
2020-04-07 23:33:44 -0700

    Add TODO comment

--- syllogism.go
+++ syllogism.go
@@ -8,6 +8,7 @@ package main
 * Use Golang list type for Line collection?
 * Improve dumping further
 * Remove any spurious whitespace in entries
+* Cache occurrences and refresh on add line and delete line
 *
 * Testing notes:
 * - Can simulate closed loop with sample, removing line 20, and then setting line 10 to "all men are mortal" (duplicating line 70)
