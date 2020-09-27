a3b8274d6665c6c2e15c2fc3021804d8dc14780a
Andrew Merenbach <andrew@merenbach.com>
2020-09-27 16:09:57 -0700

    Continue refactor

--- cmd/syllogism/syllogism.go
+++ cmd/syllogism/syllogism.go
@@ -513,166 +513,6 @@ func basicGosub2890(stringarray_s []string, intarray_t []token.Type) (form.Form,
 	return premise.PremiseForm(stringarray_s, intarray_t, &recentWord1, &recentWord2)
 }
 
-func tokenize(localstring_l1 string) ([7]string, [8]token.Type, [3]article.Type, term.Type, error) {
-	// 2020
-	//---L1$ into array S$()---
-
-	// T(): 1:line num., 2:"/", 3:quantifier, 4:no/not, 5:is/are, 6:term
-	//                     10 SOME  FRIED COCONUTS   ARE  NOT  TASTY
-	//                      1   3        6            5    4     6
-
-	// TODO: try to return when we need to return
-	//       or consider no-ops: https://blog.golang.org/error-handling-and-go
-	var returnErr error
-
-	var localstring_s string
-	var shadowstringarray_s [7]string
-	var shadowintarray_t [8]token.Type
-	var shadowintarray_e [3]article.Type
-
-	localint_p1 := term.TypeUndetermined
-	shadowintarray_e[2] = article.TypeNone
-	localint_j := 1
-
-	localint_l := len(localstring_l1)
-
-	nextToken := func() {
-		localint_j++
-	}
-	setToken := func(s string, t token.Type) {
-		shadowstringarray_s[localint_j] = s
-		shadowintarray_t[localint_j] = t
-	}
-	addTermToken := func(s string) {
-		setToken(s, token.TypeTerm)
-	}
-	closeToken := func(s string, t token.Type) {
-		setToken(s, t)
-		nextToken()
-	}
-
-	line2670 := func(tabCount int) { // 2670
-		returnErr = fmt.Errorf("%s^\nReserved word %q may not occur within a term", basicTabString(tabCount), localstring_s)
-		shadowintarray_t[1] = token.TypeReserved
-	}
-
-	// TODO: use strings.Fields here; but need to properly increase letter count
-	var localint_i int
-Iterate:
-	for _, word := range strings.Split(localstring_l1, " ") {
-		localint_i++
-
-		if word == "" {
-			continue
-		}
-
-		// find beginning of next word, skipping any spaces
-		localstring_s = word
-		localint_k := len(localstring_s)
-
-		if localint_j > 1 {
-			goto Line2520
-		}
-
-		if localstring_s == "/" {
-			closeToken(localstring_s, token.TypeSlash)
-		} else {
-			if _, err := strconv.Atoi(localstring_s); err != nil {
-				returnErr = fmt.Errorf("%s^   Invalid numeral or command", basicTabString(localint_i+len(localstring_s)))
-				break
-			}
-			closeToken(localstring_s, token.TypeLineNumber)
-		}
-		goto Line2860
-
-	Line2520: // 2520
-		// Scan
-
-		/* General overview:
-		1. If word is somebody/something/nobody/nothing/someone/everyone/everybody/everything, raise reserved word.
-		2. If word is all/some, append as type3 quantifier token if current token isn't a term, then increase counter; otherwise, raise reserved word.
-		3. If word is no/not, append as type4 no/not token if current token isn't a term, then increase counter; otherwise, raise reserved word.
-		4. If word is is/are, raise error if current token is NOT a term or if either of previous two tokens include is/are; otherwise, increase counter to close current term, THEN append as type5 is/are token as next term, then increase counter.                                                                                                                                                                                                                            5. If current token is term, append space and then word.  Do NOT increment counter.
-		6. If prev two tokens do NOT include is/are, then append this word as a type6 term token.  Do NOT increment counter.
-		7. If word is "the," set flag P1=2 (designator/definite article) and append as type6 term token.  Do NOT increment counter.
-		8. If word is NOT any of a/an/sm, append as type6 term token.  Do NOT increment counter.
-		9. If this is the LAST token, append as type6 term token.  Do NOT increment counter.
-		10. Set E[2] = index of type of article (a/an/sm) and set flag P1=1 (general term/indefinite article), then and append as type6 term token.  Do NOT increment counter.
-		11. If number of tokens > 6, end processing.
-		*/
-
-		switch localstring_s {
-		case "somebody", "something", "nobody", "nothing":
-			line2670(localint_i + localint_k - 1)
-			break Iterate
-
-		case "someone", "everyone", "everybody", "everything":
-			line2670(localint_i + localint_k - 1)
-			break Iterate
-
-		case form.WordAll, form.WordSome:
-			if shadowintarray_t[localint_j] == token.TypeTerm {
-				line2670(localint_i + localint_k - 1)
-				break Iterate
-			}
-			closeToken(localstring_s, token.TypeQuantifier)
-
-		case form.WordNo, form.WordNot:
-			if shadowintarray_t[localint_j] == token.TypeTerm {
-				line2670(localint_i + localint_k - 1)
-				break Iterate
-			}
-			closeToken(localstring_s, token.TypeNegation)
-
-		case "is", "are":
-			if shadowintarray_t[localint_j] != token.TypeTerm {
-				line2670(localint_i + localint_k - 1)
-				break Iterate
-			} else if shadowintarray_t[localint_j-1] == token.TypeCopula || shadowintarray_t[localint_j-2] == token.TypeCopula {
-				line2670(localint_i + localint_k - 1)
-				break Iterate
-			}
-			// NOTE: This is needed here, and not above, because of positioning of to-be verbs in lines.
-			// All/some and no/not will occur either at the beginning of the line or right after a to-be verb.
-			// To-be verbs are the only words to (legitimately) show up right after the end of a term token (type 6).
-			nextToken()
-			closeToken(localstring_s, token.TypeCopula)
-
-		default:
-			if shadowintarray_t[localint_j] == token.TypeTerm {
-				shadowstringarray_s[localint_j] += " " + localstring_s
-
-			} else if shadowintarray_t[localint_j-1] != token.TypeCopula && shadowintarray_t[localint_j-2] != token.TypeCopula {
-				addTermToken(localstring_s)
-
-			} else if localstring_s != article.WordA && localstring_s != article.WordAn && localstring_s != article.WordSm {
-				if localstring_s == article.WordThe {
-					// DESIGNATOR (definite article)
-					localint_p1 = term.TypeDesignator
-				}
-				addTermToken(localstring_s)
-
-			} else if localint_i == localint_l {
-				addTermToken(localstring_s)
-
-			} else {
-				shadowintarray_e[2] = article.TypeFromString(localstring_s)
-				// GENERAL TERM (indefinite article)
-				localint_p1 = term.TypeGeneralTerm
-			}
-		}
-		goto Line2860
-
-	Line2860: // 2860
-		localint_i += len(word)
-		if localint_j > 6 {
-			break
-		}
-	}
-
-	return shadowstringarray_s, shadowintarray_t, shadowintarray_e, localint_p1, returnErr
-}
-
 // TODO: errors should exit early here
 func basicGosub8980() {
 	// 8980
@@ -680,7 +520,7 @@ func basicGosub8980() {
 
 	for _, localstring_l1 := range strings.Split(help.SampleData, "\n") {
 		fmt.Println(localstring_l1)
-		stringarray_s, intarray_t, intarray_e, localint_p1, err := tokenize(localstring_l1)
+		stringarray_s, intarray_t, intarray_e, localint_p1, err := premise.Tokenize(localstring_l1)
 		if err != nil {
 			fmt.Println(err)
 		}
@@ -861,7 +701,7 @@ func runloop(localstring_l1 string) bool {
 
 	//--scan line L1$ into array S$()
 
-	stringarray_s, intarray_t, intarray_e, localint_p1, err := tokenize(localstring_l1)
+	stringarray_s, intarray_t, intarray_e, localint_p1, err := premise.Tokenize(localstring_l1)
 	if err != nil {
 		fmt.Println(err)
 	}
--- internal/premise/premise.go
+++ internal/premise/premise.go
@@ -3,11 +3,14 @@ package premise
 import (
 	"errors"
 	"fmt"
+	"strconv"
 	"strings"
 
+	"github.com/merenbach/syllogism/internal/article"
 	"github.com/merenbach/syllogism/internal/form"
 	"github.com/merenbach/syllogism/internal/help"
 	"github.com/merenbach/syllogism/internal/symbol"
+	"github.com/merenbach/syllogism/internal/term"
 	"github.com/merenbach/syllogism/internal/token"
 )
 
@@ -45,7 +48,7 @@ func New(s string) (*Premise, error) {
 	}, nil
 }
 
-// PremiseForm determines the form of a premise. This is being transitioned to not needing input.
+// PremiseForm determines the form of a premise.
 func PremiseForm(stringarray_s []string, intarray_t []token.Type, recentWord1 *string, recentWord2 *string) (form.Form, error) {
 	if stringarray_s[2] != form.WordAll {
 		if stringarray_s[2] != form.WordSome {
@@ -127,6 +130,173 @@ func PremiseForm(stringarray_s []string, intarray_t []token.Type, recentWord1 *s
 	*recentWord1 = stringarray_s[3]
 	*recentWord2 = stringarray_s[5]
 	return form.AllAIsB, nil // all A is B
+}
+
+// BasicTab prints tabs in the manner of BASIC's TAB(N)
+// This is duplicated from syllogism.go. TODO: REMOVE
+func basicTabString(n int) string {
+	return strings.Repeat(" ", n)
+}
+
+// Tokenize a string.
+func Tokenize(localstring_l1 string) ([7]string, [8]token.Type, [3]article.Type, term.Type, error) {
+	// 2020
+	//---L1$ into array S$()---
+
+	// T(): 1:line num., 2:"/", 3:quantifier, 4:no/not, 5:is/are, 6:term
+	//                     10 SOME  FRIED COCONUTS   ARE  NOT  TASTY
+	//                      1   3        6            5    4     6
+
+	// TODO: try to return when we need to return
+	//       or consider no-ops: https://blog.golang.org/error-handling-and-go
+	var returnErr error
+
+	var localstring_s string
+	var shadowstringarray_s [7]string
+	var shadowintarray_t [8]token.Type
+	var shadowintarray_e [3]article.Type
+
+	localint_p1 := term.TypeUndetermined
+	shadowintarray_e[2] = article.TypeNone
+	localint_j := 1
+
+	localint_l := len(localstring_l1)
+
+	nextToken := func() {
+		localint_j++
+	}
+	setToken := func(s string, t token.Type) {
+		shadowstringarray_s[localint_j] = s
+		shadowintarray_t[localint_j] = t
+	}
+	addTermToken := func(s string) {
+		setToken(s, token.TypeTerm)
+	}
+	closeToken := func(s string, t token.Type) {
+		setToken(s, t)
+		nextToken()
+	}
+
+	line2670 := func(tabCount int) { // 2670
+		returnErr = fmt.Errorf("%s^\nReserved word %q may not occur within a term", basicTabString(tabCount), localstring_s)
+		shadowintarray_t[1] = token.TypeReserved
+	}
+
+	// TODO: use strings.Fields here; but need to properly increase letter count
+	var localint_i int
+Iterate:
+	for _, word := range strings.Split(localstring_l1, " ") {
+		localint_i++
+
+		if word == "" {
+			continue
+		}
+
+		// find beginning of next word, skipping any spaces
+		localstring_s = word
+		localint_k := len(localstring_s)
+
+		if localint_j > 1 {
+			goto Line2520
+		}
+
+		if localstring_s == "/" {
+			closeToken(localstring_s, token.TypeSlash)
+		} else {
+			if _, err := strconv.Atoi(localstring_s); err != nil {
+				returnErr = fmt.Errorf("%s^   Invalid numeral or command", basicTabString(localint_i+len(localstring_s)))
+				break
+			}
+			closeToken(localstring_s, token.TypeLineNumber)
+		}
+		goto Line2860
+
+	Line2520: // 2520
+		// Scan
+
+		/* General overview:
+		1. If word is somebody/something/nobody/nothing/someone/everyone/everybody/everything, raise reserved word.
+		2. If word is all/some, append as type3 quantifier token if current token isn't a term, then increase counter; otherwise, raise reserved word.
+		3. If word is no/not, append as type4 no/not token if current token isn't a term, then increase counter; otherwise, raise reserved word.
+		4. If word is is/are, raise error if current token is NOT a term or if either of previous two tokens include is/are; otherwise, increase counter to close current term, THEN append as type5 is/are token as next term, then increase counter.                                                                                                                                                                                                                            5. If current token is term, append space and then word.  Do NOT increment counter.
+		6. If prev two tokens do NOT include is/are, then append this word as a type6 term token.  Do NOT increment counter.
+		7. If word is "the," set flag P1=2 (designator/definite article) and append as type6 term token.  Do NOT increment counter.
+		8. If word is NOT any of a/an/sm, append as type6 term token.  Do NOT increment counter.
+		9. If this is the LAST token, append as type6 term token.  Do NOT increment counter.
+		10. Set E[2] = index of type of article (a/an/sm) and set flag P1=1 (general term/indefinite article), then and append as type6 term token.  Do NOT increment counter.
+		11. If number of tokens > 6, end processing.
+		*/
+
+		switch localstring_s {
+		case "somebody", "something", "nobody", "nothing":
+			line2670(localint_i + localint_k - 1)
+			break Iterate
+
+		case "someone", "everyone", "everybody", "everything":
+			line2670(localint_i + localint_k - 1)
+			break Iterate
+
+		case form.WordAll, form.WordSome:
+			if shadowintarray_t[localint_j] == token.TypeTerm {
+				line2670(localint_i + localint_k - 1)
+				break Iterate
+			}
+			closeToken(localstring_s, token.TypeQuantifier)
+
+		case form.WordNo, form.WordNot:
+			if shadowintarray_t[localint_j] == token.TypeTerm {
+				line2670(localint_i + localint_k - 1)
+				break Iterate
+			}
+			closeToken(localstring_s, token.TypeNegation)
+
+		case "is", "are":
+			if shadowintarray_t[localint_j] != token.TypeTerm {
+				line2670(localint_i + localint_k - 1)
+				break Iterate
+			} else if shadowintarray_t[localint_j-1] == token.TypeCopula || shadowintarray_t[localint_j-2] == token.TypeCopula {
+				line2670(localint_i + localint_k - 1)
+				break Iterate
+			}
+			// NOTE: This is needed here, and not above, because of positioning of to-be verbs in lines.
+			// All/some and no/not will occur either at the beginning of the line or right after a to-be verb.
+			// To-be verbs are the only words to (legitimately) show up right after the end of a term token (type 6).
+			nextToken()
+			closeToken(localstring_s, token.TypeCopula)
+
+		default:
+			if shadowintarray_t[localint_j] == token.TypeTerm {
+				shadowstringarray_s[localint_j] += " " + localstring_s
+
+			} else if shadowintarray_t[localint_j-1] != token.TypeCopula && shadowintarray_t[localint_j-2] != token.TypeCopula {
+				addTermToken(localstring_s)
+
+			} else if localstring_s != article.WordA && localstring_s != article.WordAn && localstring_s != article.WordSm {
+				if localstring_s == article.WordThe {
+					// DESIGNATOR (definite article)
+					localint_p1 = term.TypeDesignator
+				}
+				addTermToken(localstring_s)
+
+			} else if localint_i == localint_l {
+				addTermToken(localstring_s)
+
+			} else {
+				shadowintarray_e[2] = article.TypeFromString(localstring_s)
+				// GENERAL TERM (indefinite article)
+				localint_p1 = term.TypeGeneralTerm
+			}
+		}
+		goto Line2860
+
+	Line2860: // 2860
+		localint_i += len(word)
+		if localint_j > 6 {
+			break
+		}
+	}
+
+	return shadowstringarray_s, shadowintarray_t, shadowintarray_e, localint_p1, returnErr
 }
 
 // ContrastingTerm returns the opposite term in a premise.
